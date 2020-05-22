2df0cb97de04208160b521fae712ee885a3f002b
HoNguyenThanhVinh <honguyenthanhvinh@gmail.com>
2020-05-22 13:39:00 +0700

    2020-05-22 13:30

--- Events/forms.py
+++ Events/forms.py
@@ -4,6 +4,6 @@ from django import forms
 
 
 class QueryForm(forms.Form):
-    fromDate = forms.DateField(required=True, widget=forms.TextInput(attrs={'type': 'date'}), label='Từ ngày')
+    fromDate = forms.DateField(required=True, widget=forms.TextInput(attrs={'type': 'date'}), label='Từ ngày (*)')
     toDate = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), label='Đến ngày', required=False)
     incident = forms.BooleanField(widget=forms.CheckboxInput(), label='Có sự cố', required=False)
